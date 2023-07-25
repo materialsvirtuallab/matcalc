e68ecbb41ada352926088ef1255f90c7e4a82bf6
Shyue Ping Ong <sp@ong.ai>
2023-07-25 09:19:20 -0700

    Add calc_many method.

--- matcalc/base.py
+++ matcalc/base.py
@@ -5,6 +5,8 @@ import abc
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from collections.abc import Generator, Sequence
+
     from pymatgen.core import Structure
 
 
@@ -22,3 +24,17 @@ class PropCalc(metaclass=abc.ABCMeta):
 
         Returns: {"prop name": value}
         """
+
+    def calc_many(self, structures: Sequence[Structure]) -> Generator[dict, None, None]:
+        """
+        Performs calc on many structures. The return type is a generator given that the calc method can potentially be
+        reasonably expensive. It is trivial to convert the generator to a list/tuple.
+
+        Args:
+            structures: List or generator of Structures
+
+        Returns:
+            Generator of dicts.
+        """
+        for structure in structures:
+            yield self.calc(structure)
