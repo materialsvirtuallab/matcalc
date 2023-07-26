8c3e6b9a1e1da10e0c6fd43324718f0c13fb3f8e
Shyue Ping Ong <sp@ong.ai>
2023-07-26 07:35:02 -0700

    Get version of matcalc.

--- matcalc/__init__.py
+++ matcalc/__init__.py
@@ -1,1 +1,9 @@
 """Calculators for materials properties."""
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("matcalc")
+except PackageNotFoundError:
+    pass  # package not installed
