e4cff57cc9086b9ce6ac35cd9480d68a2ff20c26
Shyue Ping Ong <sp@ong.ai>
2023-08-07 20:38:01 -0700

    Fix missing comma.

--- tests/test_util.py
+++ tests/test_util.py
@@ -5,7 +5,7 @@ from ase.calculators.calculator import Calculator
 
 
 def test_get_calculator():
-    for name in ("M3GNet" "M3GNet-MP-2021.2.8-PES", "CHGNet"):
+    for name in ("M3GNet", "M3GNet-MP-2021.2.8-PES", "CHGNet"):
         calc = get_calculator(name)
         assert isinstance(calc, Calculator)
     with pytest.raises(ValueError, match="Unsupported model name"):
