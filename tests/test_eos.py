7be0eb53d9a648fed826e3f39b7375d13b5006a7
Shyue Ping Ong <shyuep@gmail.com>
2023-08-10 13:36:35 -0700

    Fix test regression.

--- tests/test_eos.py
+++ tests/test_eos.py
@@ -10,11 +10,11 @@ def test_PhononCalc(Li2O, LiFePO4, M3GNetUPCalc):
     """Tests for PhononCalc class"""
     calculator = M3GNetUPCalc
     # Note that the fmax is probably too high. This is for testing purposes only.
-    pcalc = EOSCalc(calculator)
+    pcalc = EOSCalc(calculator, fmax=0.01)
     results = pcalc.calc(Li2O)
 
     assert results["bulk_modulus"] == pytest.approx(69.86879801931632, 1e-2)
 
     results = list(pcalc.calc_many([Li2O, LiFePO4]))
     assert len(results) == 2
-    assert results[1]["bulk_modulus"] == pytest.approx(51.70659653126171, 1e-2)
+    assert results[1]["bulk_modulus"] == pytest.approx(53.791519828150925, 1e-2)
