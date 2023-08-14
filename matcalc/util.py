1da878d2c377bc385efa06726dde07f13702d490
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-08-14 14:10:49 -0700

    more concise get_universal_calculator doc str

--- matcalc/util.py
+++ matcalc/util.py
@@ -10,10 +10,11 @@ UNIVERSAL_CALCULATORS = ("M3GNet-MP-2021.2.8-PES", "M3GNet-MP-2021.2.8-DIRECT-PE
 @functools.lru_cache
 def get_universal_calculator(name: str, **kwargs):
     """
-    Helper method to get some well-known **universal** calculators. Note that imports should be within the if
-    statements to ensure that all these are optional. It should be stressed that this method is for **universal**
-    calculators encompassing a wide swath of the periodic table only. Though matcalc can be used with any MLIP, even
-    custom ones, it is not the intention for this method to provide a listing of all MLIPs.
+    Helper method to get some well-known **universal** calculators.
+    Imports should be inside if statements to ensure that all models are optional dependencies.
+    All calculators must be universal, i.e. encompass a wide swath of the periodic table.
+    Though matcalc can be used with any MLIP, even custom ones, this function is not meant as
+        a list of all MLIPs.
 
     Args:
         name (str): Name of calculator.
