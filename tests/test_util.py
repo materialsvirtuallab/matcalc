dc3f731f3ffd35302545b7d67bedca5d8d43ad11
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-08-15 11:58:06 -0700

    fix test_get_universal_calculator

--- tests/test_util.py
+++ tests/test_util.py
@@ -4,10 +4,13 @@ from matcalc.util import get_universal_calculator, UNIVERSAL_CALCULATORS
 from ase.calculators.calculator import Calculator
 
 
-def test_get_calculator():
+def test_get_universal_calculator():
     for name in UNIVERSAL_CALCULATORS:
         calc = get_universal_calculator(name)
         assert isinstance(calc, Calculator)
+        same_calc = get_universal_calculator(calc)  # test ASE Calculator classes are returned as-is
+        assert calc is same_calc
 
-    with pytest.raises(ValueError, match="Unsupported name='whatever'"):
+    with pytest.raises(ValueError) as exc:
         get_universal_calculator("whatever")
+    assert str(exc.value) == f"Unrecognized name='whatever', must be one of {UNIVERSAL_CALCULATORS}"
