5e4fc5aafa3423ddee40d00cbfa6959378380d23
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-11-18 07:45:30 -0800

    ElasticityCalc keep type hints and document defaults

--- matcalc/elasticity.py
+++ matcalc/elasticity.py
@@ -24,8 +24,8 @@ class ElasticityCalc(PropCalc):
     def __init__(
         self,
         calculator: Calculator,
-        norm_strains=(0.001, 0.003, 0.005, 0.01),
-        shear_strains=(0.001, 0.003, 0.005, 0.01),
+        norm_strains: tuple[float, ...] = (0.001, 0.003, 0.005, 0.01),
+        shear_strains: tuple[float, ...] = (0.001, 0.003, 0.005, 0.01),
         fmax: float = 0.1,
         relax_structure: bool = True,
         use_equilibrium: bool = False,
@@ -34,10 +34,14 @@ class ElasticityCalc(PropCalc):
         Args:
             calculator: ASE Calculator to use.
             norm_strains: strain values to apply to each normal mode.
+                Defaults to (0.001, 0.003, 0.005, 0.01).
             shear_strains: strain values to apply to each shear mode.
-            fmax: maximum force in the relaxed structure (if relax_structure).
+                Defaults to (0.001, 0.003, 0.005, 0.01).
+            fmax: maximum force in the relaxed structure (if relax_structure). Defaults to 0.1.
             relax_structure: whether to relax the provided structure with the given calculator.
+                Defaults to True.
             use_equilibrium: whether to use the equilibrium stress and strain.
+                Defaults to False.
         """
         self.calculator = calculator
         self.norm_strains = norm_strains
@@ -46,7 +50,7 @@ class ElasticityCalc(PropCalc):
         self.fmax = fmax
         self.use_equilibrium = use_equilibrium
 
-    def calc(self, structure: Structure) -> dict:
+    def calc(self, structure: Structure) -> dict[str, float | ElasticTensor | Structure]:
         """
         Calculates elastic properties of Pymatgen structure with units determined by the calculator,
         (often the stress_weight).
@@ -65,8 +69,8 @@ class ElasticityCalc(PropCalc):
         }
         """
         if self.relax_structure:
-            rcalc = RelaxCalc(self.calculator, fmax=self.fmax)
-            structure = rcalc.calc(structure)["final_structure"]
+            relax_calc = RelaxCalc(self.calculator, fmax=self.fmax)
+            structure = relax_calc.calc(structure)["final_structure"]
 
         adaptor = AseAtomsAdaptor()
         deformed_structure_set = DeformedStructureSet(
@@ -80,19 +84,20 @@ class ElasticityCalc(PropCalc):
             atoms.calc = self.calculator
             stresses.append(atoms.get_stress(voigt=False))
 
-        strains = [
-            Strain.from_deformation(deformation)
-            for deformation in deformed_structure_set.deformations
-        ]
+        strains = [Strain.from_deformation(deformation) for deformation in deformed_structure_set.deformations]
         atoms = adaptor.get_atoms(structure)
         atoms.calc = self.calculator
         if self.use_equilibrium:
             elastic_tensor, residuals_sum = self._elastic_tensor_from_strains(
-                strains, stresses, eq_stress=atoms.get_stress(voigt=False),
+                strains,
+                stresses,
+                eq_stress=atoms.get_stress(voigt=False),
             )
         else:
             elastic_tensor, residuals_sum = self._elastic_tensor_from_strains(
-                strains, stresses, eq_stress=None,
+                strains,
+                stresses,
+                eq_stress=None,
             )
         return {
             "elastic_tensor": elastic_tensor,
@@ -120,24 +125,20 @@ class ElasticityCalc(PropCalc):
         for all of the linear fits done to compute the entries of the tensor.
         """
         strain_states = [tuple(ss) for ss in np.eye(6)]
-        ss_dict = get_strain_state_dict(
-            strains, stresses, eq_stress=eq_stress, add_eq=self.use_equilibrium
-        )
+        ss_dict = get_strain_state_dict(strains, stresses, eq_stress=eq_stress, add_eq=self.use_equilibrium)
         if not set(strain_states) <= set(ss_dict):
-            raise ValueError(
-                f"Missing independent strain states: {set(strain_states) - set(ss_dict)}"
-            )
+            raise ValueError(f"Missing independent strain states: {set(strain_states) - set(ss_dict)}")
         if len(set(ss_dict) - set(strain_states)) > 0:
             warnings.warn(
-                "Extra strain states in strain-stress pairs are neglected in independent strain fitting"
+                "Extra strain states in strain-stress pairs are neglected in independent strain fitting", stacklevel=1
             )
         c_ij = np.zeros((6, 6))
         residuals_sum = 0
         for i in range(6):
-            istrains = ss_dict[strain_states[i]]["strains"]
-            istresses = ss_dict[strain_states[i]]["stresses"]
+            strain = ss_dict[strain_states[i]]["strains"]
+            stress = ss_dict[strain_states[i]]["stresses"]
             for j in range(6):
-                fit = np.polyfit(istrains[:, i], istresses[:, j], 1, full=True)
+                fit = np.polyfit(strain[:, i], stress[:, j], 1, full=True)
                 c_ij[i, j] = fit[0][0]
                 residuals_sum += fit[1][0]
         c = ElasticTensor.from_voigt(c_ij)
