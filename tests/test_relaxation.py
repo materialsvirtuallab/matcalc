4e3ad4ac7352bdeab7806d9376cbb0c3d4038c03
PROA200 <85151701+PROA200@users.noreply.github.com>
2023-11-29 01:25:22 -0800

    Rewrite tests without LiFePO4

--- tests/test_relaxation.py
+++ tests/test_relaxation.py
@@ -5,20 +5,20 @@ import pytest
 from matcalc.relaxation import RelaxCalc
 
 
-def test_relax_calc(LiFePO4, M3GNetCalc, tmp_path):
-    pcalc = RelaxCalc(M3GNetCalc, traj_file=f"{tmp_path}/lfp_relax.txt", optimizer="FIRE")
-    results = pcalc.calc(LiFePO4)
-    assert results["a"] == pytest.approx(4.75571137)
-    assert results["b"] == pytest.approx(6.13161423)
-    assert results["c"] == pytest.approx(10.4385933)
-    assert results["alpha"] == pytest.approx(90, abs=1)
-    assert results["beta"] == pytest.approx(90, abs=1)
-    assert results["gamma"] == pytest.approx(90, abs=1)
-    assert results["volume"] == pytest.approx(results["a"] * results["b"] * results["c"], abs=0.1)
+def test_relax_calc(Li2O, M3GNetCalc, tmp_path):
+    pcalc = RelaxCalc(M3GNetCalc, traj_file=f"{tmp_path}/li2o_relax.txt", optimizer="FIRE")
+    results = pcalc.calc(Li2O)
+    assert results["a"] == pytest.approx(3.291071792359756, rel=0.002)
+    assert results["b"] == pytest.approx(3.291071899625086, rel=0.002)
+    assert results["c"] == pytest.approx(3.291072056855788, rel=0.002)
+    assert results["alpha"] == pytest.approx(60, abs=1)
+    assert results["beta"] == pytest.approx(60, abs=1)
+    assert results["gamma"] == pytest.approx(60, abs=1)
+    assert results["volume"] == pytest.approx(results["a"] * results["b"] * results["c"]/2**0.5, abs=0.1)
 
-    results = list(pcalc.calc_many([LiFePO4] * 2))
+    results = list(pcalc.calc_many([Li2O] * 2))
     assert len(results) == 2
-    assert results[-1]["a"] == pytest.approx(4.7557113)
+    assert results[-1]["a"] == pytest.approx(3.291071792359756, rel=0.002)
 
     with pytest.raises(ValueError, match="Unknown optimizer='invalid', must be one of "):
         RelaxCalc(M3GNetCalc, optimizer="invalid")
