330fecca907746f2f78dfeee86807c3ece285484
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-12-12 10:00:55 -0800

    Fix `get_universal_calculator` AttributeError: 'SumCalculator' object has no attribute 'lower' (#15)
    * fix get_universal_calculator AttributeError: 'SumCalculator' object has no attribute 'lower'
    
    from running
    get_universal_calculator("mace", model="large", default_dtype="float64", dispersion=True)
    
    * breaking rename matcalc/(util -> utils).py
    
    * call get_universal_calculator() outside NEB images loop
    
    * add test case for non-str/non-calc input in get_universal_calculator()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import pytest
 from ase.calculators.calculator import Calculator
 
-from matcalc.util import UNIVERSAL_CALCULATORS, get_universal_calculator
+from matcalc.utils import UNIVERSAL_CALCULATORS, get_universal_calculator
 
 
 def test_get_universal_calculator() -> None:
@@ -17,3 +17,7 @@ def test_get_universal_calculator() -> None:
     with pytest.raises(ValueError, match=f"Unrecognized {name=}") as exc:
         get_universal_calculator(name)
     assert str(exc.value) == f"Unrecognized {name=}, must be one of {UNIVERSAL_CALCULATORS}"
+
+    # cover edge case like https://github.com/materialsvirtuallab/matcalc/issues/14
+    # where non-str and non-ASE Calculator instances are passed in
+    assert get_universal_calculator(42) == 42  # test non-str input is returned as-is
