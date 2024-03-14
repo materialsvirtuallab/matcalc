f69fd93961b8c47e0ad0f605b5d7f12e8603f60f
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-03-14 06:40:55 +0100

    test write_phonon in test_phonon_calc()

--- tests/test_phonon.py
+++ tests/test_phonon.py
@@ -1,6 +1,8 @@
 """Tests for PhononCalc class"""
+
 from __future__ import annotations
 
+import os
 from typing import TYPE_CHECKING
 
 import pytest
@@ -8,15 +10,25 @@ import pytest
 from matcalc.phonon import PhononCalc
 
 if TYPE_CHECKING:
+    from pathlib import Path
+
     from matgl.ext.ase import M3GNetCalculator
     from pymatgen.core import Structure
 
 
-def test_phonon_calc(Li2O: Structure, M3GNetCalc: M3GNetCalculator) -> None:
+def test_phonon_calc(Li2O: Structure, M3GNetCalc: M3GNetCalculator, tmp_path: Path) -> None:
     """Tests for PhononCalc class"""
     # Note that the fmax is probably too high. This is for testing purposes only.
+
+    # change dir to tmp_path
+    phonon_yaml = tmp_path / "phonon.yaml"
     phonon_calc = PhononCalc(
-        M3GNetCalc, supercell_matrix=((2, 0, 0), (0, 2, 0), (0, 0, 2)), fmax=0.1, t_step=50, t_max=1000
+        M3GNetCalc,
+        supercell_matrix=((2, 0, 0), (0, 2, 0), (0, 0, 2)),
+        fmax=0.1,
+        t_step=50,
+        t_max=1000,
+        write_phonon=phonon_yaml,
     )
     result = phonon_calc.calc(Li2O)
 
@@ -28,3 +40,5 @@ def test_phonon_calc(Li2O: Structure, M3GNetCalc: M3GNetCalculator) -> None:
 
     results = list(phonon_calc.calc_many([Li2O, Li2O]))
     assert len(results) == 2
+
+    assert os.path.isfile(phonon_yaml)
