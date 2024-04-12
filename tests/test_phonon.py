cf1147a77c861e0334c678337123f6a3a13c39db
Runze Liu <146490083+rul048@users.noreply.github.com>
2024-04-11 17:32:34 -0700

    Covered the other new keywords
    Signed-off-by: Runze Liu <146490083+rul048@users.noreply.github.com>

--- tests/test_phonon.py
+++ tests/test_phonon.py
@@ -16,18 +16,24 @@ if TYPE_CHECKING:
     from pymatgen.core import Structure
 
 
-def test_phonon_calc(Li2O: Structure, M3GNetCalc: M3GNetCalculator, tmp_path: Path) -> None:
+def test_phonon_calc(Li2O: Structure, M3GNetCalc: M3GNetCalculator, force_constants_tmp_path: Path, band_structure_tmp_path: Path, total_dos_tmp_path: Path, phonon_tmp_path: Path) -> None:
     """Tests for PhononCalc class"""
     # Note that the fmax is probably too high. This is for testing purposes only.
 
     # change dir to tmp_path
-    phonon_yaml = tmp_path / "phonon.yaml"
+    force_constants = force_constants_tmp_path / "force_constants"
+    band_structure_yaml = band_structure_tmp_path / "band_structure.yaml"
+    total_dos_dat = total_dos_tmp_path / "total_dos.dat"
+    phonon_yaml = phonon_tmp_path / "phonon.yaml"
     phonon_calc = PhononCalc(
         M3GNetCalc,
         supercell_matrix=((2, 0, 0), (0, 2, 0), (0, 0, 2)),
         fmax=0.1,
         t_step=50,
         t_max=1000,
+        write_force_constants=force_constants,
+        write_band_structure=band_structure_yaml,
+        write_total_dos=total_dos_dat,
         write_phonon=phonon_yaml,
     )
     result = phonon_calc.calc(Li2O)
@@ -41,4 +47,7 @@ def test_phonon_calc(Li2O: Structure, M3GNetCalc: M3GNetCalculator, tmp_path: Pa
     results = list(phonon_calc.calc_many([Li2O, Li2O]))
     assert len(results) == 2
 
+    assert os.path.isfile(force_constants)
+    assert os.path.isfile(band_structure_yaml)
+    assert os.path.isfile(total_dos_dat)
     assert os.path.isfile(phonon_yaml)
