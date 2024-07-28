09226eda02137dcb8305907d46da5c25b8248db8
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-07-28 07:16:36 -0400

    split test_relax_calc into test_relax_calc_(single|many) and parametrize over ExpCellFilter + FrechetCellFilter

--- tests/test_eos.py
+++ tests/test_eos.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pytest
-from ase.constraints import ExpCellFilter
+from ase.filters import ExpCellFilter
 
 from matcalc.eos import EOSCalc
 
--- tests/test_relaxation.py
+++ tests/test_relaxation.py
@@ -3,45 +3,48 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pytest
-from ase.filters import ExpCellFilter
+from ase.filters import ExpCellFilter, FrechetCellFilter
 
 from matcalc.relaxation import RelaxCalc
 
 if TYPE_CHECKING:
     from pathlib import Path
 
+    from ase.filters import Filter
     from matgl.ext.ase import M3GNetCalculator
     from pymatgen.core import Structure
 
 
-def test_relax_calc(Li2O: Structure, M3GNetCalc: M3GNetCalculator, tmp_path: Path) -> None:
+@pytest.mark.parametrize(("cell_filter", "expected_a"), [(ExpCellFilter, 3.291071), (FrechetCellFilter, 3.288585)])
+def test_relax_calc_single(
+    Li2O: Structure, M3GNetCalc: M3GNetCalculator, tmp_path: Path, cell_filter: Filter, expected_a: float
+) -> None:
     relax_calc = RelaxCalc(
-        M3GNetCalc, traj_file=f"{tmp_path}/li2o_relax.txt", optimizer="FIRE", cell_filter=ExpCellFilter
+        M3GNetCalc, traj_file=f"{tmp_path}/li2o_relax.txt", optimizer="FIRE", cell_filter=cell_filter
     )
     result = relax_calc.calc(Li2O)
-    assert result["a"] == pytest.approx(3.291071792359756, rel=0.002)
-    assert result["b"] == pytest.approx(3.291071899625086, rel=0.002)
-    assert result["c"] == pytest.approx(3.291072056855788, rel=0.002)
-    assert result["alpha"] == pytest.approx(60, abs=1)
-    assert result["beta"] == pytest.approx(60, abs=1)
-    assert result["gamma"] == pytest.approx(60, abs=1)
-    assert result["volume"] == pytest.approx(result["a"] * result["b"] * result["c"] / 2**0.5, abs=0.1)
+    final_struct: Structure = result["final_structure"]
+    missing_keys = {*final_struct.lattice.params_dict} - {*result}
+    assert len(missing_keys) == 0, f"{missing_keys=}"
+    a, b, c, alpha, beta, gamma = final_struct.lattice.parameters
 
+    assert a == pytest.approx(expected_a, rel=0.002)
+    assert b == pytest.approx(expected_a, rel=0.002)
+    assert c == pytest.approx(expected_a, rel=0.002)
+    assert alpha == pytest.approx(60, abs=1)
+    assert beta == pytest.approx(60, abs=1)
+    assert gamma == pytest.approx(60, abs=1)
+    assert final_struct.volume == pytest.approx(a * b * c / 2**0.5, abs=0.1)
+
+
+@pytest.mark.parametrize(("cell_filter", "expected_a"), [(ExpCellFilter, 3.291071), (FrechetCellFilter, 3.288585)])
+def test_relax_calc_many(Li2O: Structure, M3GNetCalc: M3GNetCalculator, cell_filter: Filter, expected_a: float) -> None:
+    relax_calc = RelaxCalc(M3GNetCalc, optimizer="FIRE", cell_filter=cell_filter)
     results = list(relax_calc.calc_many([Li2O] * 2))
     assert len(results) == 2
-    assert results[-1]["a"] == pytest.approx(3.291071792359756, rel=0.002)
+    assert results[-1]["a"] == pytest.approx(expected_a, rel=0.002)
 
-    relax_calc_frechet = RelaxCalc(M3GNetCalc, optimizer="FIRE")
-    result_frechet = relax_calc_frechet.calc(Li2O)
-    assert result_frechet["a"] == pytest.approx(3.288585037486192, rel=0.002)
-    assert result_frechet["b"] == pytest.approx(3.288586588568963, rel=0.002)
-    assert result_frechet["c"] == pytest.approx(3.2885877985629786, rel=0.002)
-    assert result_frechet["alpha"] == pytest.approx(60, abs=1)
-    assert result_frechet["beta"] == pytest.approx(60, abs=1)
-    assert result_frechet["gamma"] == pytest.approx(60, abs=1)
-    assert result_frechet["volume"] == pytest.approx(
-        result_frechet["a"] * result_frechet["b"] * result_frechet["c"] / 2**0.5, abs=0.1
-    )
 
+def test_relax_calc_invalid_optimizer(M3GNetCalc: M3GNetCalculator) -> None:
     with pytest.raises(ValueError, match="Unknown optimizer='invalid', must be one of "):
         RelaxCalc(M3GNetCalc, optimizer="invalid")
