139e3487ba097f7082cfc1ea4e65f64c259e772a
Shyue Ping Ong <shyuep@gmail.com>
2025-03-14 10:41:20 -0700

    Fix accuracy in approx float test.

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -13,4 +13,4 @@ def test_elasticity_benchmark(M3GNetCalc: M3GNetCalculator) -> None:
     benchmark = ElasticityBenchmark(n_samples=10)
     results = benchmark.run(M3GNetCalc, "toy")
     assert len(results) == 10
-    assert results["AE K_toy"].mean() == pytest.approx(65.20042336543436)
+    assert results["AE K_toy"].mean() == pytest.approx(65.20042336543436, abs=1e-1)
