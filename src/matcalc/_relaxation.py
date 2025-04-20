c563bfac0695a2d4864c2b0c60c1568f8d91594e
rul048 <rul048@ucsd.edu>
2025-04-19 19:11:50 -0700

    Fix the mypy

--- src/matcalc/_relaxation.py
+++ src/matcalc/_relaxation.py
@@ -203,7 +203,7 @@ class RelaxCalc(PropCalc):
         structure_in: Structure = result["final_structure"].copy()
 
         if self.perturb_distance is not None:
-            structure_in = structure_in.perturb(distance=self.perturb_distance, seed=None)
+            structure_in = structure_in.perturb(distance=self.perturb_distance, seed=None) # type: ignore[arg-type]
         atoms = AseAtomsAdaptor.get_atoms(structure_in)
         atoms.calc = self.calculator
         if self.relax_atoms:
