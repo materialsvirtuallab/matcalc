1a4040d0b14d9466aced778f6497ce7eb921968a
Shyue Ping Ong <shyuep@gmail.com>
2025-05-29 09:34:25 -0700

    iFix type errors.

--- src/matcalc/utils.py
+++ src/matcalc/utils.py
@@ -138,7 +138,7 @@ class PESCalculator(Calculator):
         system_changes = system_changes or all_changes
         super().calculate(atoms=atoms, properties=properties, system_changes=system_changes)
 
-        structure: Structure | IStructure = AseAtomsAdaptor.get_structure(atoms)  # type: ignore[arg-type]
+        structure: Structure | IStructure = AseAtomsAdaptor.get_structure(atoms)  # type: ignore[arg-type,assignment]
         efs_calculator = EnergyForceStress(ff_settings=self.potential)
         energy, forces, stresses = efs_calculator.calculate([structure])[0]
 
