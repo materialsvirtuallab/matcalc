c79a281117c8eeb6e5dfd979a9a0553109be7803
Andrew S. Rosen <asrosen93@gmail.com>
2025-08-12 13:42:05 -0400

    Update _md.py
    Signed-off-by: Andrew S. Rosen <asrosen93@gmail.com>

--- src/matcalc/_md.py
+++ src/matcalc/_md.py
@@ -10,14 +10,13 @@ from ase import units
 from ase.md import Langevin
 from ase.md.andersen import Andersen
 from ase.md.bussi import Bussi
-from ase.md.nose_hoover_chain import IsotropicMTKNPT, NoseHooverChainNVT
+from ase.md.nose_hoover_chain import IsotropicMTKNPT, MTKNPT, NoseHooverChainNVT
 from ase.md.npt import NPT
 from ase.md.nptberendsen import Inhomogeneous_NPTBerendsen, NPTBerendsen
 from ase.md.nvtberendsen import NVTBerendsen
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase.md.verlet import VelocityVerlet
 from packaging.version import Version
-
 from ._base import PropCalc
 from ._relaxation import RelaxCalc
 from .backend._ase import TrajectoryObserver
@@ -288,10 +287,6 @@ class MDCalc(PropCalc):
                 append_trajectory=self.append_trajectory,
             )
         if ensemble == "npt_mtk":
-            if Version(_ase_version) > Version("3.25.0"):
-                from ase.md.nose_hoover_chain import MTKNPT  # type:ignore[attr-defined]
-            else:
-                raise ImportError("MTKNPT is only available in ASE version 3.26.0 or later.")
             return MTKNPT(
                 atoms,
                 timestep=timestep_fs,
