e35b9329cae19b8c2b778dbf27e1bfd184222d86
Andrew S. Rosen <asrosen93@gmail.com>
2025-08-15 13:13:26 -0400

    Allow for `RelaxCalc` to be run with `relax_cell=True`
    Closes #95. Tests are still needed. Others are welcome to help with that if they feel so inclined; otherwise, I'll do so when I have a spare moment.
    
    Signed-off-by: Andrew S. Rosen <asrosen93@gmail.com>

--- src/matcalc/_md.py
+++ src/matcalc/_md.py
@@ -373,14 +373,9 @@ class MDCalc(PropCalc):
         if self.relax_structure:
             # Create a RelaxCalc instance with the specified calculator, convergence criteria (fmax),
             # optimizer, and any additional keyword arguments for the relaxation calculation.
-            relaxer = RelaxCalc(
-                self.calculator,
-                fmax=self.fmax,
-                optimizer=self.optimizer,
-                relax_atoms=True,
-                relax_cell=False,
-                **(self.relax_calc_kwargs or {}),
-            )
+            full_relax_calc_kwargs = {"fmax": self.fmax, "optimizer": self.optimizer, "relax_atoms": True, "relax_cell": False} | (self.relax_calc_kwargs or {})
+            
+            relaxer = RelaxCalc(self.calculator, **relax_calc_kwargs)
             # Run the relaxation calculation and update the result dictionary.
             result |= relaxer.calc(structure_in)
             # Update the input structure with the relaxed final structure.
