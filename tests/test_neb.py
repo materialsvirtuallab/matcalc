c029800fcb1efbea51f1a899688650fb75bcfc54
Shyue Ping Ong <shyuep@gmail.com>
2025-11-13 11:51:54 -0800

    Fix MD and NEB tests.

--- tests/test_neb.py
+++ tests/test_neb.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from matcalc import NEBCalc
+from matcalc import NEBCalc, RelaxCalc
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -15,14 +15,17 @@ if TYPE_CHECKING:
 
 def test_neb_calc(LiFePO4: Structure, matpes_calculator: PESCalculator, tmp_path: Path) -> None:
     """Tests for NEBCalc class"""
+    relax = RelaxCalc(matpes_calculator, fmax=0.5, relax_cell=False)
     image_start = LiFePO4.copy()
     image_start.remove_sites([2])
+    image_start = relax.calc(image_start)["final_structure"]
     image_end = LiFePO4.copy()
     image_end.remove_sites([3])
+    image_end = relax.calc(image_end)["final_structure"]
     neb_calc = NEBCalc(matpes_calculator, traj_folder=tmp_path, fmax=0.5)
     barriers = neb_calc.calc_images(image_start, image_end, n_images=5)
-    assert barriers["barrier"] == pytest.approx(0.0184783935546875, rel=0.002)
-    assert barriers["force"] == pytest.approx(0.0018920898, rel=0.002)
+    assert barriers["barrier"] == pytest.approx(0.17913818359375044, rel=0.002)
+    assert barriers["force"] == pytest.approx(-0.0042724609375, rel=0.002)
     assert isinstance(barriers["mep"], dict), "barriers['mep'] should be a dictionary"
     for key, value in barriers["mep"].items():
         assert "structure" in value, f"Missing 'structure' key in barriers['mep'][{key}]"
